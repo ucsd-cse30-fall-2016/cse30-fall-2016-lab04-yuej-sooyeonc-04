35f5611a5a1c10263fce8dcfbe9de70e016e7e78
schough <schough@ucsd.edu>
2016-11-25 14:51:28 -0800

    Create str_to_int.c

---
+++ str_to_int.c
@@ -0,0 +1,20 @@
+/* ********************************
+ * CSE 30 - HW 4
+ *
+ * Sooyeon E. Chough (A92093139)
+ * Yue Jiang (A92095681)
+ * ********************************/
+
+/* int str_to_int(char * s, int * dest)
+*  Args: 
+*       char * s: the string to change into int
+*       int * dest: where to place the int
+*  Returns:
+*       int: 1 if successful OR 0 if failed
+*           meaning either of the arguments are null
+*           or s is not a valid string of int
+*/
+int str_to_int(char * s, int * dest)
+{
+    return 0;
+}
