10ea714c3d2e86bd354b50ec0ea7344309018aba
schough <schough@ucsd.edu>
2016-11-25 15:13:47 -0800

    Update str_to_int.c

--- str_to_int.c
+++ str_to_int.c
@@ -16,5 +16,30 @@
 */
 int str_to_int(char * s, int * dest)
 {
+    // Making sure neither are null
+    if( s == NULL || dest == NULL )
+    {
+        return 0;
+    }
+    // Getting length of string and initializing other variables
+    int length = strlen( s );
+    int total, i = 0;
+    // Checking first char for - sign
+    if( s[ i ] != 45 && (s[ i ] < 48 || s[ i ] > 57) )
+    {
+        return 0;
+    }       
+    // Making sure that s is all 0 - 9
+    for( i = 1; i < length; i++ )
+    {
+        if( s[ i ] < 48 || s[ i ] > 57 )
+        {
+            return 0;
+        }
+    }
+    // Getting the number
+    for( i = length; i > 0; i-- )
+    {
+    }
     return 0;
 }
