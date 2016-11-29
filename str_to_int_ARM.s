123bdec9f570ee2768d8a6dcec2a8bc2815513c1
yuej <yuj062@ucsd.edu>
2016-11-29 02:40:40 -0800

    Update str_to_int_ARM.s

--- str_to_int_ARM.s
+++ str_to_int_ARM.s
@@ -28,9 +28,34 @@ str_to_int:
     BEQ end
     
     CMP r1, #0
-    BEQ end @if either parameter is NULL, return 0
+    BEQ end       @if either parameter is NULL, return 0
     
-    BL ss
+    BL strlen     @length = strlen(s)
+    MOV r2, r0    @now r2 stores length
+    CMP r2, #1
+    BLT end       @if length < 1, return 0
+    
+    LDR r3, [r0]  @r3 temporarily stores the first value of the input string
+    CMP r3, #45
+    BNE first_value_comparison
+    
+    MOV r4, #0    @i = 0
+    CMP r4, r2    @i < length @QUESTION: why in your c code it's i < length instead of i<=length?
+    BLE loop
+    
+loop:
+    LDRB r3, [r0, r4]
+    CMP r3, #48
+    BLT end
+    CMP r3, #57
+    BGT end
+    ADD r4, r4, #1
+    CMP r4, r2
+    BLE loop
+
+out_of_loop:
+    
+    
     /* Return back to calling function */
     mov r0, #0
     @ This handles restoring registers and returning
