a1d0e4be53b946cea76f69f62de38d01f81afd81
schough <schough@ucsd.edu>
2016-11-29 13:23:24 -0800

    Update str_to_int_test.c

--- str_to_int_test.c
+++ str_to_int_test.c
@@ -1,9 +1,6 @@
 /* ********************************
  * CSE 30 - HW 4
  *
- * This file is merely designed to give you an idea how to compile/test your own
- * code. You may modify it as you like.
- *
  * Sooyeon E. Chough (A92093139)
  * Yue Jiang (A92095681)
  * ********************************/
@@ -14,14 +11,13 @@ int str_to_int(char * str, int * dest);
 
 int main(int argc, char ** argv) {
     
-    /**int x;
+    int x, a;
+    
     if(str_to_int("123", &x)) {
         printf("%d\n", x);
     } else {
         printf("Invalid integer string\n");
-    }*/
-    
-    int a;
+    }
     
     str_to_int( "-012", &a );
     assert( a == -012 );
