89d8ac3517bb76e24acb62d3c3175c0c52f590c6
schough <schough@ucsd.edu>
2016-11-30 13:27:37 -0800

    Update substring.s

--- substring.s
+++ substring.s
@@ -12,86 +12,84 @@
 
 @ int substring(char * s1, char * s2)
 substring:
+
     @ Save caller's registers on the stack
     push {r4-r11, ip, lr}
 
     @-----------------------
 
-    @ r8 is count for s1
-    MOV r8, #0
-    @ r9 is count for s2
+    @ r6 is count1 for s1
+    MOV r6, #0
+    @ r9 is count2 for s2
     MOV r9, #0
+    
     @ r4 is s1
     MOV r4, r0
-    @ r6 is s2
-    MOV r6, r1
+    @ r7 is s2
+    MOV r7, r1
+    
     @ r5 is length of s1
     MOV r0, r4
     BL strlen
     MOV r5, r0
-    @ r7 is length of s2
-    MOV r0, r6
+    @ r8 is length of s2
+    MOV r0, r7
     BL strlen
-    MOV r7, r0
+    MOV r8, r0
+    
 checkEmpty1:
     @ check if s1 is empty string
     CMP r5, #0
     BNE checkEmpty2
     @ if an empty string, return true
-    MOV r0, #1
-    B end
+    B endTrue
 checkEmpty2:
     @ check if s2 is empty string
     CMP r7, #0
-    BNE notEmpty
+    BNE lengthComparison
     @ if an empty string, return true
-    MOV r0, #1
-    B end
-notEmpty:
-    @ getting first chars of arrays
-    @ r0 is s1[ 0 ]
-    LDR r0, [r4, r8]
-    @ r1 is s2[ 0 ]
-    LDR r1, [r6, r9]
-loop:
-checkNull1:
-    @ check if s1 is null
-    CMP r0, #0
-    BNE checkNull2
-    MOV r0, #0
-    B end
-checkNull2:
-    @ check if s2 is null
-    CMP r1, #0
-    BNE notNull
-    MOV r0, #0
-    B end
-notNull:
-    @
+    B endTrue
 
-@ TODO // Does not account for when reach end of s1 AND is a substring (aka when null and right)
+lengthComparison:
+    @ comparing the lengths of the two strings
+    CMP r5, r7
+    BLE shorterFirst
+    @ reordering so that shorter s is first
+    MOV r0, r7
+    MOV r1, r8
+    MOV r2, r9
+    MOV r7, r4
+    MOV r8, r5
+    MOV r9, r6
+    MOV r4, r0
+    MOV r5, r1
+    MOV r6, r2
+shorterFirst:
+    @ now, shorter s is r4 - r6 (now s1)
+    @ and, longer s is r7 - r9 (now s2)
 
-checkLength1:
-    @ check if end of loop
-    CMP r8, r5
-    BNE checkLength2
-    MOV r0, #0
-    B end
-checkLength2:
-    @ check if end of loop
-    CMP r9, r7
-    BNE loopAgain
-    MOV r0, #0
-    B end
-loopAgain:
-    @ not end of loop so increment
+@ finding where s1[ 0 ] = s2[ count2 ]
+searchLoopStart:
+    @ loop condition 
+    CMP r9, r8
+    LGE endFail 
+    @ getting char from string
+    @ r0 is s1[ count1 ]
+    LDR r0, [ r4, r8 ]
+    @ r1 is s2[ count2 ]
+    LDR r1, [ r7, r9 ]
+    
+    @ incrementing count2 
     ADD r9, r9, #1
-    @ and get next char
-    @ r0 is s1[ count ]
-    LDR r0, [r4, r8]
-    @ r1 is s2[ count ]
-    LDR r1, [r6, r9]
-    B loop
+searchLoopDone:
+    
+@ if true or false
+endFail:
+    MOV r0, #0
+    B end
+endTrue:
+    MOV r0, #1
+    
 end:
 
     @-----------------------
@@ -101,6 +99,7 @@ end:
 
     @ ARM equivalent of return
     BX lr
+    
 .endfunc
 
 .end
